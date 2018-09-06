be2336d11cd6e146056c81bd8cd4be8a4422b505
Brian Wong <brian.wong@duke.edu>
2018-09-06 15:52:50 -0400

    added xiao's ee task mgr function

---
+++ py/task_count_mgr.py
@@ -0,0 +1,28 @@
+def maximum_no_of_tasks(MaxNActive, waitingPeriod):
+ 
+    """maintain a maximum number of active tasks
+    
+    _source: Xiao Yang, UNC Postdoc at Global Hydrology Lab, https://github.com/seanyx"""
+
+    time.sleep(10)
+
+    ## initialize submitting jobs
+    ts = list(ee.batch.Task.list())
+
+    NActive = 0
+    for task in ts:
+            if ('RUNNING' in str(task) or 'READY' in str(task)):
+                    NActive += 1
+
+    ## wait if the number of current active tasks reach the maximum number
+    ## defined in MaxNActive
+
+    while (NActive >= MaxNActive):
+
+            time.sleep(waitingPeriod) # if reach or over maximum no. of active tasks, wait for 2min and check again
+            ts = list(ee.batch.Task.list())
+            NActive = 0
+            for task in ts:
+                    if ('RUNNING' in str(task) or 'READY' in str(task)):
+                            NActive += 1
+    return()
